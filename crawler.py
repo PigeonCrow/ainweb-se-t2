7cd00efce0ec9192acfddd4abf43fe87647fc464
TITO DUARTE <ic-consult.duarte@extaccount.com>
2024-12-01 18:34:36 +0100

    define basic crawler

--- crawler.py
+++ crawler.py
@@ -1,10 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
-# def Pull requests
-
+# def Pull requests -->  GET
+url = "https://vm009.rz.uos.de/crawl/index.html"
+response = requests.get(url)
 
 # process with bs4
+soup = BeautifulSoup(response.content, "html.parser")
 
 # parse requests
+title = soup.find("h1").text
+paragraph = soup.find("p").text
+
+
+print(f"Title: {title}")
+print("\n")
+print(f"Paragraph: {paragraph}")
