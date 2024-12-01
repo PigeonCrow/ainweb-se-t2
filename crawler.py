88767ff7633c552700dd4f220979820a4190bd1b
TITO DUARTE <ic-consult.duarte@extaccount.com>
2024-12-01 20:51:33 +0100

    find all link elements

--- crawler.py
+++ crawler.py
@@ -4,15 +4,20 @@ from bs4 import BeautifulSoup
 # def Pull requests -->  GET
 url = "https://vm009.rz.uos.de/crawl/index.html"
 response = requests.get(url)
-
 # process with bs4
 soup = BeautifulSoup(response.content, "html.parser")
 
 # parse requests
-title = soup.find("h1").text
-paragraph = soup.find("p").text
+title = soup.find("h1")
+paragraph = soup.find("p")
+links = soup.find_all("a")
+
+# create full urls with links
 
 
-print(f"Title: {title}")
+print(f"Title: {title.text}")
 print("\n")
-print(f"Paragraph: {paragraph}")
+print(f"Paragraph: {paragraph.text}")
+print("\n")
+for l in links:
+    print(f"link: {l.text}")
