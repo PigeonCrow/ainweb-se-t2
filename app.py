f4f88e81f288e77312196a0ca311fbfcd5f2b971
TITO DUARTE <ic-consult.duarte@extaccount.com>
2025-01-17 17:10:13 +0100

    cleanup

--- app.py
+++ app.py
@@ -2,61 +2,11 @@ from flask import Flask, request, render_template, url_for
 from search import init_search, search  # change for server deployment
 import traceback
 
+# globally initialize search
 init_search()
 
 # building the flask app
 app = Flask(__name__)
-# ix = open_dir("index_dir")
-
-
-# config setup
-# DEFAULT_CONFIG = {
-#    "root_url": "https://vm009.rz.uos.de/crawl/index.html",
-#   "periodic_crawl": False
-# }
-
-# def load_config():
-#    if os.path.exists("config.json"):
-#    with open("config.json", "r") as file:
-#        return json.load(file)
-# else:
-#    print("Config file not found. Using defaults.")
-#    return DEFAULT_CONFIG
-
-
-# initiazie app
-# def init_set_up():
-#    print("Setting UP SE ....")
-
-#    url = load_config.root_url
-#    if not os.path.exists("index_dir/"):
-#        os.makedirs("index_dir")
-#
-#    print("setting up Index .....")
-#
-#    # define schema
-#    schema = Schema(
-#        url=ID(stored=True),
-#        title=TEXT(stored=True),
-#        teaser=TEXT(stored=True),
-#        content=TEXT,
-#    )
-#
-#    # generate the index
-#    ix = create_in("index_dir", schema)
-#
-#    # Initialize writer and visited_urls set
-#    writer = ix.writer()
-#    visited_urls = set()
-#
-#    # Start the crawling process
-#    crawl(url, url, visited_urls, writer)  # perform crawling once while setting up
-#
-#    # Commit the changes to the index
-#    writer.commit()
-
-#    if periodic_crawl:
-#    start_periodic_crawl() # start periodic crawling if enabled
 
 
 # home route of the flask app
